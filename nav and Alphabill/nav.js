6059ebed231945c17b6fe7654055c9e5ba34213c
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-05-04 05:30:26 -0400

    blog commit

---
+++ nav and Alphabill/nav.js
@@ -0,0 +1,4 @@
+document.querySelector('.toggle').addEventListener('click', ()=>{
+    document.querySelector('.toggle').classList.toggle('close')
+document.querySelector('.mobile-nav').classList.toggle('display')
+})
